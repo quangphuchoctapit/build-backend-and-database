ad622b61419df18cc9278258c6f5f28a079b5fef
Fuc le <quangphuchoctapit@gmail.com>
2023-07-27 01:16:21 +0700

    expressjs with mvc

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,12 @@
 import express from "express"
+import homeController from '../controllers/homeController'
+import homeAbout from '../controllers/homeController'
 
 let router = express.Router()
 
 let initWebRoutes = app => {
-    router.get("/", (req, res) => {
-        return res.send("Hello world with ok")
-    })
+    router.get("/", homeController.getHomePage)
+    router.get('/about', homeAbout.getAbout)
     router.get("/hoidanit", (req, res) => {
         return res.send("Hello world with ok hoidanit")
     })
