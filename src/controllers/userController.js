132e432135406a0ed5a03f354eb95db13cc2120e
Fuc le <quangphuchoctapit@gmail.com>
2023-08-15 16:41:45 +0700

    write APIs to retrieve user in4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -20,6 +20,25 @@ let handleLogin = async (req, res) => {
     })
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing parameter',
+            users: []
+        })
+    }
+    let users = await userService.getAllUsers(id)
+    console.log(users)
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
+}
+
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 }
