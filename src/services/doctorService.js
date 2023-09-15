ed6ebb1c4c05d49155f1833952848bf2937500f4
Fuc le <quangphuchoctapit@gmail.com>
2023-09-15 10:47:56 +0700

    design interface displaying detail doctor react

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -98,11 +98,17 @@ const getDetailDoctorById = (inputId) => {
                     }
                     ],
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary')
+
+
+                }
+                if (!data) data = {}
                 resolve({
                     errCode: 0,
                     data: data
